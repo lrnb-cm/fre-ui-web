a82b6bf878760513208b7406ed82007ae7a7152a
Zach <zfarley94@gmail.com>
2021-07-01 14:41:43 -0400

    # This is a combination of 2 commits. # This is the 1st commit message:
    add basic next package
    
    add basic next package
    
    add eslint
    
    add lint js config
    
    format doc
    
    add script to fix format of files
    
    format with prettier
    
    update format script
    
    add jest config
    
    add circleci config
    
    update config
    
    use lint script
    
    correct file name
    
    rename file
    
    rm lock file
    
    test results
    
    pkg merge fix
    
    add basic next package
    
    rm old files
    
    # This is the commit message #2:
    
    add basic next package

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  reporters: ["default", "jest-junit"],
+  "jest-junit": {
+    suiteName: "jest tests",
+    outputDirectory: ".",
+    outputName: "junit.xml",
+    uniqueOutputName: "false",
+    classNameTemplate: "{classname}-{title}",
+    titleTemplate: "{classname}-{title}",
+    ancestorSeparator: " › ",
+    usePathForSuiteName: "true",
+  },
+};
