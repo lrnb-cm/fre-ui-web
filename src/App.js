eab5e21546dc4623dc2c89e4229c484347bf4d65
ayorich <kayodele17@yahoo.com>
2022-06-16 10:06:42 +0100

    ui-styled

--- src/App.js
+++ src/App.js
@@ -1,57 +1,68 @@
-import { StrictMode } from 'react'
-import { BrowserRouter as Router, useRoutes, Route, Routes } from 'react-router-dom'
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
-import { uri } from './config'
-import LoginCallback from './components/loginContainer/Callback'
-import DashboardContainer from './components/dashboardContainer/DashboardContainer'
-import { Containers, LoginContainer } from './components'
-import './App.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import { ThemeProvider, StyledEngineProvider, Theme } from '@mui/material/styles'
-import { CssBaseline } from '@mui/material'
-import { theme, customTheming } from './theme'
-import Layout from './components/Layout'
-import { deepmerge } from '@mui/utils'
-import { createTheme } from '@mui/material/styles'
-import { FirebaseContext } from './contexts/FirebaseContext'
-import { initializeApp } from 'firebase/app'
-import Customer from './components/customerContainer/Customers'
-import ProductsContainer from './components/productsContainer/ProductsContainer'
-import ReportDetailsContainer from './components/reportDetailsContainer/ReportDetails'
-import MyShopContainer from './components/myshopContainer/MyShopContainer'
-import MyShopDetailsContainer from './components/myShopDetailsContainer/MyShopDetailsContainer'
-import { 
-  CUSTOMERS, 
-  VALIDATED, 
-  REPORT_DETAILS, 
-  REPORT, 
-  DASHBOARD, 
-  PRODUCTS, 
-  TRANSACTIONS, 
-  MY_SHOP, 
-  SHOP_DETAILS 
-} from './constants/routes'
+import { StrictMode } from 'react';
+import {
+  BrowserRouter as Router,
+  useRoutes,
+  Route,
+  Routes,
+} from 'react-router-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { uri } from './config';
+import LoginCallback from './components/loginContainer/Callback';
+import DashboardContainer from './components/dashboardContainer/DashboardContainer';
+import { Containers, LoginContainer } from './components';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  ThemeProvider,
+  StyledEngineProvider,
+  Theme,
+} from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
+import { theme, customTheming } from './theme';
+import Layout from './components/Layout';
+import { deepmerge } from '@mui/utils';
+import { createTheme } from '@mui/material/styles';
+import { FirebaseContext } from './contexts/FirebaseContext';
+import { initializeApp } from 'firebase/app';
+import Customer from './components/customerContainer/Customers';
+import ProductsContainer from './components/productsContainer/ProductsContainer';
+import ReportDetailsContainer from './components/reportDetailsContainer/ReportDetails';
+import MyShopContainer from './components/myshopContainer/MyShopContainer';
+import MyShopDetailsContainer from './components/myShopDetailsContainer/MyShopDetailsContainer';
+import {
+  CUSTOMERS,
+  VALIDATED,
+  REPORT_DETAILS,
+  REPORT,
+  DASHBOARD,
+  PRODUCTS,
+  TRANSACTIONS,
+  MY_SHOP,
+  SHOP_DETAILS,
+} from './constants/routes';
+
+import './App.css';
+
 const firebaseConfig = {
   apiKey: 'AIzaSyAb2yKgDGJowDNhEugINyMyjqBry8c-nBI',
-  authDomain: 'freeing-returns.firebaseapp.com'
-}
+  authDomain: 'freeing-returns.firebaseapp.com',
+};
 
 export default function App() {
   // @ts-ignore
   if (module.hot) {
     // @ts-ignore
     module.hot.accept('./components', (e) => {
-      const PageComponent = require('./components')
-      render(main(PageComponent), appRootElement)
-    })
+      const PageComponent = require('./components');
+      render(main(PageComponent), appRootElement);
+    });
   }
   // setup apollo
   const client = new ApolloClient({
     uri,
-    cache: new InMemoryCache()
-  })
-  const mergeTheme = createTheme(deepmerge(theme, customTheming))
-  const firebase = initializeApp(firebaseConfig)
+    cache: new InMemoryCache(),
+  });
+  const mergeTheme = createTheme(deepmerge(theme, customTheming));
+  const firebase = initializeApp(firebaseConfig);
   return (
     <StrictMode>
       <StyledEngineProvider injectFirst>
@@ -67,10 +78,16 @@ export default function App() {
                     <Route index element={<DashboardContainer />} />
                     <Route path={CUSTOMERS} element={<Customer />} />
                     <Route path={PRODUCTS} element={<ProductsContainer />} />
-                    <Route path={REPORT_DETAILS} element={<ReportDetailsContainer />} />
+                    <Route
+                      path={REPORT_DETAILS}
+                      element={<ReportDetailsContainer />}
+                    />
                     <Route path={TRANSACTIONS} element={<Containers />} />
                     <Route path={MY_SHOP} element={<MyShopContainer />} />
-                    <Route path={SHOP_DETAILS} element={<MyShopDetailsContainer />} />
+                    <Route
+                      path={SHOP_DETAILS}
+                      element={<MyShopDetailsContainer />}
+                    />
                     {/* Using path="*"" means "match anything", so this route
                     acts like a catch-all for URLs that we don't have explicit
                     routes for. */}
@@ -83,5 +100,5 @@ export default function App() {
         </ApolloProvider>
       </StyledEngineProvider>
     </StrictMode>
-  )
+  );
 }
--- src/components/datePicker/DatePicker.tsx
+++ src/components/datePicker/DatePicker.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { DatePicker } from 'antd';
 import { styled } from '@mui/material/styles';
 import { MinusOutlined } from '@ant-design/icons';
+import { Moment } from 'moment';
 import 'antd/dist/antd.css';
 const { RangePicker } = DatePicker;
 
 export default function CustomDatePicker() {
+  const [open, setOpen] = useState(false);
+  const onDateChange = (dates: any, dateStrings: [string, string]) => {
+    console.log('onDateChange', dates, dateStrings);
+    setOpen(false);
+  };
+
+  const onCalendarChange = (
+    dates: any,
+    dateStrings: [string, string],
+    info: any
+  ) => {
+    console.log('onCalendarChange', dates, dateStrings, info);
+  };
+
   const panelRender = (
     originalPanel:
       | boolean
@@ -18,7 +33,20 @@ export default function CustomDatePicker() {
     return (
       <div style={{ display: 'flex' }}>
         <div>{originalPanel}</div>
-        <span>extra me</span>
+        <ExtraPanel>
+          <SelectionTile>Today</SelectionTile>
+          <SelectionTile>Yesterday</SelectionTile>
+          <SelectionTile>Last 7 Days</SelectionTile>
+          <SelectionTile>Last Week</SelectionTile>
+          <SelectionTile>Last 2 Weeks</SelectionTile>
+          <SelectionTile>This Month</SelectionTile>
+          <SelectionTile>Last Month</SelectionTile>
+          <SelectionTile>Custom Range</SelectionTile>
+          <ButtonWrapper>
+            <ApplyBtn>Apply</ApplyBtn>
+            <CancelBtn>Cancel</CancelBtn>
+          </ButtonWrapper>
+        </ExtraPanel>
       </div>
     );
   };
@@ -28,14 +56,18 @@ export default function CustomDatePicker() {
       <RangePicker
         renderExtraFooter={() => 'extra footer'}
         size="small"
-        format={'MM/DD'}
-        // open={false}
+        format={'MMM-DD'}
+        open={open}
         panelRender={panelRender}
         bordered={false}
         placeholder={['Start', 'End']}
         separator={
           <MinusOutlined style={{ fontSize: '6px', color: '#6c757d' }} />
         }
+        onCalendarChange={onCalendarChange}
+        onChange={onDateChange}
+        onFocus={() => setOpen(true)}
+        dropdownClassName="calendarPop"
       />
     </DateWrapper>
   );
@@ -47,5 +79,47 @@ const DateWrapper = styled('div')(({ theme }) => ({
   },
   '& .ant-picker': {
     width: '150px !important',
+  },
+}));
+
+const ExtraPanel = styled('div')(({ theme }) => ({
+  padding: '5px 10px',
+  width: '160px',
+}));
+
+const SelectionTile = styled('div')(({ theme }) => ({
+  border: '1px solid #e4e9ef',
+  margin: '5px 0px',
+  padding: '5px',
+  borderRadius: '4px',
+  cursor: 'pointer',
+  '&:hover': {
+    border: '1px solid #c4cfdc',
+  },
+}));
+
+const ButtonWrapper = styled('div')(({ theme }) => ({
+  marginTop: '10px',
+  display: 'flex',
+  justifyContent: 'space-between',
+}));
+
+const ApplyBtn = styled('div')(({ theme }) => ({
+  backgroundColor: '#3758CC',
+  padding: '5px 15px',
+  borderRadius: '4px',
+  cursor: 'pointer',
+  color: '#fff',
+  '&:hover': {},
+}));
+
+const CancelBtn = styled('div')(({ theme }) => ({
+  border: '1px solid #e4e9ef',
+  padding: '5px 10px',
+  borderRadius: '4px',
+  cursor: 'pointer',
+
+  '&:hover': {
+    border: '1px solid #c4cfdc',
   },
 }));
